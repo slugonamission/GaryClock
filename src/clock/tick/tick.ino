93473551149cfc7441c9153e1dcc22e14e84c031
Russell Joyce <russell.joyce@york.ac.uk>
2014-06-08 22:51:20 +0100

    Fixed multiple meter ticking delays

--- src/clock/tick/tick.ino
+++ src/clock/tick/tick.ino
@@ -5,6 +5,7 @@
 #define SECS_PIN  9
 #define MINS_PIN  10
 #define HOURS_PIN 11
+int allHands[] = {SECS_PIN, MINS_PIN, HOURS_PIN};
 
 #define TICK_TIME 1000
 
@@ -18,6 +19,12 @@
 void setup() { 
   // Let the needle settle at 0
   delay(1000);
+
+/*
+  moveDamped(HOURS_PIN, 0, 127);
+  moveDamped(MINS_PIN, 0, 127);
+  moveDamped(SECS_PIN, 0, 127);
+*/
 } 
 
 
@@ -28,7 +35,7 @@ void loop() {
 
   // 2.5V ticks
   ticks(2);
-  
+ 
   // 1V ticks
   ticks(5);
 
@@ -39,15 +46,13 @@ void loop() {
   ticks(20);
 
   // 0.125V ticks
-  ticks(40);
+  //ticks(40);
 
 
   // Ticking clock test - 0 to 59 then reset
   ticksUp(59);
   #if(USE_DAMPING)
-    moveDamped(HOURS_PIN, 255, 0);
-    moveDamped(MINS_PIN, 255, 0);
-    moveDamped(SECS_PIN, 255, 0);
+    moveMultipleDamped(allHands, 3, 255, 0);
     delay(1000 - DAMP_DURATION);
   #else
     analogWrite(HOURS_PIN, 0);
@@ -65,9 +70,7 @@ void loop() {
     delay(235);
   }
   #if(USE_DAMPING)
-    moveDamped(HOURS_PIN, 255, 0);
-    moveDamped(MINS_PIN, 255, 0);
-    moveDamped(SECS_PIN, 255, 0);
+    moveMultipleDamped(allHands, 3, 255, 0);
     delay(235 - DAMP_DURATION);
   #else
     analogWrite(HOURS_PIN, 0);
@@ -88,9 +91,7 @@ void ticks(int count) {
 void ticksUp(int count) {
   for(int fadeValue = 1; fadeValue <= count; fadeValue++) {
     #if(USE_DAMPING)
-      moveDamped(HOURS_PIN, (fadeValue - 1) * 255/count, fadeValue * 255/count);
-      moveDamped(MINS_PIN, (fadeValue - 1) * 255/count, fadeValue * 255/count);
-      moveDamped(SECS_PIN, (fadeValue - 1) * 255/count, fadeValue * 255/count);
+      moveMultipleDamped(allHands, 3, (fadeValue - 1) * 255/count, fadeValue * 255/count);
       delay(TICK_TIME - DAMP_DURATION);
     #else
       analogWrite(HOURS_PIN, fadeValue * 255/count);
@@ -105,9 +106,7 @@ void ticksUp(int count) {
 void ticksDown(int count) {
   for(int fadeValue = count - 1; fadeValue >= 0; fadeValue--) { 
     #if(USE_DAMPING)
-      moveDamped(HOURS_PIN, (fadeValue + 1) * 255/count, fadeValue * 255/count);
-      moveDamped(MINS_PIN, (fadeValue + 1) * 255/count, fadeValue * 255/count);
-      moveDamped(SECS_PIN, (fadeValue + 1) * 255/count, fadeValue * 255/count);
+      moveMultipleDamped(allHands, 3, (fadeValue + 1) * 255/count, fadeValue * 255/count);
       delay(TICK_TIME - DAMP_DURATION);
     #else
       analogWrite(HOURS_PIN, fadeValue * 255/count);
@@ -127,5 +126,18 @@ void moveDamped(int pin, int currentPosition, int desiredPosition) {
   analogWrite(pin, desiredPosition + damping);
   delay(DAMP_AMOUNT);
   analogWrite(pin, desiredPosition);
+}
+
+void moveMultipleDamped(int pins[], int count, int currentPosition, int desiredPosition) {
+  int damping = currentPosition - desiredPosition;
+
+  for (int i = 0; i < count; i++)
+    analogWrite(pins[i], desiredPosition);
+  delay(DAMP_DELAY);
+  for (int i = 0; i < count; i++)
+    analogWrite(pins[i], desiredPosition + damping);
+  delay(DAMP_AMOUNT);
+  for (int i = 0; i < count; i++)
+    analogWrite(pins[i], desiredPosition);
 }
 
